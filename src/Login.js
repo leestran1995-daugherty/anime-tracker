5d3d9f7f2f79779b4a6275d071c206e5fa2efe88
Lee Tran <leetran@msp42lst061022.daugherty.com>
2020-02-07 14:47:53 -0600

    minorly style login button

--- src/Login.js
+++ src/Login.js
@@ -3,7 +3,7 @@ import React from "react";
 const Login = () => {
   return (
     <a
-      className="button"
+      className="button is-info"
       href="https://discordapp.com/api/oauth2/authorize?client_id=675021562692239380&redirect_uri=http%3A%2F%2Flocalhost%3A3000&response_type=code&scope=identify"
     >
       Login
